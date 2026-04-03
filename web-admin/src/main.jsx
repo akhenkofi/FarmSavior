554ddddd4428ade442db3565fbbe59b3dd839905
Akhenaten <kofiakhen@gmail.com>
2026-04-03 08:39:30 -0700

    Force fresh client rollout for Phone-pane mobile CSS hotfix

--- web-admin/src/main.jsx
+++ web-admin/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
-const BUILD_ID = '2026-04-03-call-push-v1'
+const BUILD_ID = '2026-04-03-phone-pane-v2'
 
 async function forceFreshClient() {
   try {
