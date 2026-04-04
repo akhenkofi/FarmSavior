1ebf274897c2c5d7647aeabaac755248e3562fc3
Akhenaten <kofiakhen@gmail.com>
2026-04-04 05:56:46 -0700

    Add mobile push-call scaffolding and immediate offer push trigger

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
 
-void main() {
+import 'screens/home_screen.dart';
+import 'services/call_push_service.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp();
+    await CallPushService.init();
+  } catch (e) {
+    debugPrint('Push init skipped: $e');
+  }
   runApp(const FarmSaviorApp());
 }
 
